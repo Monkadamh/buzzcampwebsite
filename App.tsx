e087429f20336e5d9c3ad01f092aed3f9090ce0f
Monkadamh <theadamhassam@gmail.com>
2026-06-12 01:03:57 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,40 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Articles from "./pages/Articles";
+import About from "./pages/About";
+import WriteForUs from "./pages/WriteForUs";
+import Contact from "./pages/Contact";
+import ArticlePage from "./pages/ArticlePage";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <ThemeProvider defaultTheme="light">
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/write" element={<WriteForUs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/article/:id" element={<ArticlePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </ThemeProvider>
+);
+
+export default App;
